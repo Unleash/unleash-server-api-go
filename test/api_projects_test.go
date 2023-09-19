a4a90a42b98fafbb9c7f33f39a834b89a52f08b4
Gastón Fournier <gaston@getunleash.io>
2023-09-19 09:34:56 +0200

    Add tests for add access to project

--- test/api_projects_test.go
+++ test/api_projects_test.go
@@ -44,4 +44,22 @@ func Test_client_ProjectsAPIService(t *testing.T) {
 		}
 	})
 
+	t.Run("Test ProjectsAPIService AddAccessToProject", func(t *testing.T) {
+		if enterpriseEnvironmentAvailable() {
+			projectId := "default"
+			adminUser := 1
+			memberRole := 5
+			roles := make([]int32, 1)
+			roles[0] = int32(memberRole)
+			users := make([]int32, 1)
+			users[0] = int32(adminUser)
+			groups := make([]int32, 0)
+			addAccess := client.NewProjectAddAccessSchema(roles, groups, users)
+			httpRes, err := apiClient.ProjectsAPI.AddAccessToProject(context.Background(), projectId).ProjectAddAccessSchema(*addAccess).Execute()
+
+			require.Nil(t, err)
+			assert.Equal(t, 200, httpRes.StatusCode)
+		}
+	})
+
 }
